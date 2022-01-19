2ad1be4913ec5405bc8d0a672d8834124ab9462a
ninobomba <45675003+ninobomba@users.noreply.github.com>
2022-01-19 01:07:13 -0600

    Update mvn-deploy.sh

--- scripts/mvn-deploy.sh
+++ scripts/mvn-deploy.sh
@@ -3,20 +3,20 @@
 cleanup()
 {
   echo "Executing maven home cleanup for this package"
-  mvn dependency:purge-local-repository -DmanualInclude="tech.tools4monkeys:commons:1.0"
-  rm -rf ~/.m2/repository/tech/tools4monkeys
+  mvn dependency:purge-local-repository -DmanualInclude="io.github.ninobomba:t4m-commons:1.0"
+  rm -rf ~/.m2/repository/io/github/ninobomba
 }
 
 package()
 {
   echo "Packing jar file"
-  mvn clean package -DskipTests
+  mvn clean package install -DskipTests
 }
 
 deploy()
 {
   echo "Deploying jar file"
-  mvn install:install-file -Dfile=target/t4m-commons-1.0.jar -DgroupId=tech.tools4monkeys.commons -DartifactId=t4m-commons -Dversion=1.0 -Dpackaging=jar -DgeneratedPom=true -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false -DupdateReleaseInfo=true
+  mvn install:install-file -Dfile=target/t4m-commons-1.0.jar -DgroupId=io.github.ninobomba -DartifactId=t4m-commons -Dversion=1.0 -Dpackaging=jar -DgeneratedPom=true -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false -DupdateReleaseInfo=true
 }
 
 #################################################
