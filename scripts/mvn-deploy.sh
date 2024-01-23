6ef28ecac42755f1094b61e1d012d59ef888c3aa
ninobomba <fernando.farfan@live.com>
2024-01-23 11:41:26 -0600

    FFR-Update 24/01/2024 v2

--- scripts/mvn-deploy.sh
+++ scripts/mvn-deploy.sh
@@ -2,14 +2,24 @@
 
 # https://central.sonatype.com/artifact/io.github.ninobomba/t4m-commons/versions
 
+# this script
 # Remember to update the version on each line
-# use gpg --gen-key to generate a key
-# edit maven settings.xml and add
+
+# GPG key
+# https://central.sonatype.org/pages/working-with-pgp-signatures.html
+# gpg --gen-key
+# gpg --list-secret-keys --keyid-format=long
+# gpg --keyserver keyserver.ubuntu.com --send-keys <key-id>
+# gpg --keyserver https://keys.openpgp.org --send-keys <key-id>
+
+# Maven settings.xml
+# https://central.sonatype.org/pages/apache-maven.html
 # <server>
 #   <id>ossrh</id>
 #   <username>your-jira-id</username>
 #   <password>your-jira-pwd</password>
 #  </server>
+#  https://issues.sonatype.org/login.jsp?os_destination=%2Fsecure%2FSignup%21default.jspa
 
 cleanup()
 {
